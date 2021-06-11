4bfd14988ea267aacffb0db35c9fcfd4f1d9f0e3
apex4245 <55520238+apex4245@users.noreply.github.com>
2021-06-11 17:01:47 -0400

    Added movement and damage
    Created "destObj" destructible object script.
    Created "playerController" script with basic movement. Attemp to make keycard grabbing function
    Created "keycard" script. Keycards can be referred to by their color or number, they should be interchangeable.

---
+++ GMTK Game Jam Project/Assets/Code/Apex/keycard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycard : MonoBehaviour
+{
+    [SerializeField] private int keyID;
+
+    private int blue;
+    private int green;
+    private int red;
+    private int yellow;
+
+    // Each keycard has an interchangable number or color that can be used to identify it
+    //Unity hates me tho and I'm sure there's a better way to do this
+
+    void Start()
+    {
+        /*
+        blue=keyID 1;
+        green= keyID 2;
+        red= keyID 3;
+        yellow= keyID 4;
+        */
+    }
+
+}
---
+++ GMTK Game Jam Project/Assets/Code/Apex/playerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private bool isGameOver;
+    private bool isSporeForm;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private int moveSpeed=10;
+    private Vector2 movement;
+    public float movey;
+
+    private bool hasKey1;
+    private bool hasKey2;
+    private bool hasKey3;
+    private bool hasKey4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+        isSporeForm = false;
+        hasKey1 = false; hasKey2 = false; hasKey3 = false; hasKey4 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movey = movement.x = Input.GetAxisRaw("Horizontal");
+        movey = movement.y = Input.GetAxisRaw("Vertical");
+
+        
+    }
+
+    //Used to grab keycard
+    void GrabKeycard()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Pressed E");
+            Debug.Log("Grabbed Keycard");
+            //need a way to differentiate which keycard the player is picking up
+            //TODO: fix this, only works for key1
+            hasKey1 = true;
+        }
+    }
+
+    //movement
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movey * moveSpeed, rb.velocity.y);
+       
+        Vector2 direction = movement.normalized;
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    //Collision triggers
+    //so far, for keycard
+    private void OnCollisionStay(Collision col)
+    {
+        destObj _col = col.collider.gameObject.GetComponent<destObj>();
+
+        if (col.gameObject.CompareTag("Keycard"))
+        {
+            GrabKeycard();
+            //destroy keycard 
+            //TODO: (doesn't work yet, needs to be worded differently?)
+            //DestroyObject();
+
+        }
+    }
+}
